c9e37419d1a65ae7de4ad02b0a51fb5d7f22e94b
Abinaya-R-2005 <rnavanitha2005@gmail.com>
2025-12-19 13:06:28 +0530

    Initial login page

---
+++ src/components/Login.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import "./Login.css";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    if (email === "admin@gmail.com" && password === "admin123") {
+      alert("Login Successful!");
+    } else {
+      alert("Invalid Email or Password");
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <h2>User Login</h2>
+
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Enter Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+
+        <input
+          type="password"
+          placeholder="Enter Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
